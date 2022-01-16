ea987fa964583086c1102d7d5d3d54e8ae8a804d
Seungwoo Yoo <ryuryu10@naver.com>
2022-01-17 02:29:39 +0900

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import json
+
+def init():
+    pass
+
+def main():
+    print("1. 404 체커")
+    print("2. console 확인")
+    print("><><><><><><><><><><><")
+    input_data = input("NUMBER --> ")
+    if input_data == "1":
+        console()
+
+def console():
+    driver=webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://store.leagueoflegends.co.kr/loot')
+    driver.implicitly_wait(50)
+    driver.quit()
+
+while True:
+    main()
